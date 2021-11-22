ed0f0b0ff0755bb224af54d5b39e0f60c098325b
zzz <978125451@qq.com>
2021-11-23 01:22:50 +0800

    kmp

--- kmp.rs
+++ kmp.rs
@@ -1,32 +1,6 @@
 
 fn main() {
-    let i = kmp(r"fn kmp(s: &str, p: &str) -> isize {
-        let next = get_next(p);
-        let ch_s: Vec<char> = s.chars().collect();
-        let ch_p: Vec<char> = p.chars().collect();
-        let (len_s, len_p) = (s.len(), p.len());
-        let (mut p_s, mut p_p) = (0, 0);
-        loop {
-            if len_s - p_s < len_p - p_p {
-                return -1;
-            } else {
-                if p_p == len_p - 1 && ch_p[p_p] == ch_s[p_s] {
-                    break;
-                } else {
-                    if ch_p[p_p] == ch_s[p_s] {
-                        p_s += 1;
-                        p_p += 1;
-                    } else {
-                        p_p = next[p_p];
-                        if ch_p[p_p] != ch_s[p_s] && p_p == 0 {
-                            p_s += 1;
-                        }
-                    }
-                }
-            }
-        }
-        p_s as isize - len_p as isize + 1
-    }","kmp");
+    let i = kmp("aabaababbabaaabaaaabbaaaabaab","babba");
     println!("{}",i);
 }
 
@@ -47,7 +21,9 @@ fn kmp(s: &str, p: &str) -> isize {
                     p_s += 1;
                     p_p += 1;
                 } else {
-                    p_p = next[p_p];
+                    if p_p > 0 {
+                        p_p = next[p_p]-1;
+                    }
                     if ch_p[p_p] != ch_s[p_s] && p_p == 0 {
                         p_s += 1;
                     }
@@ -57,17 +33,24 @@ fn kmp(s: &str, p: &str) -> isize {
     }
     p_s as isize - len_p as isize + 1
 }
-
 fn get_next(p: &str) -> Vec<usize> {
     let c: Vec<char> = p.chars().collect();
     let length = c.len();
     let mut next = vec![0; length];
-    for i in 1..length {
-        next[i] = if (i as isize - 2 >= 0) && c[i - 1] == c[next[i - 1]] {
-            next[i - 2] + 1
-        } else {
-            0
+    next[1] = 1;
+    let mut i = 2;
+    let mut j = 0;
+    while i < length {
+        j = next[i-1]-1;
+        if c[i-1] == c[j]{
+            next[i] = next[i-1] + 1;
+        }else{
+            while !(j == 0 || c[j] == c[i-1]){
+                j = next[j] - 1;
+            }
+            next[i] = j + 1;
         }
+        i += 1;
     }
     next
 }
